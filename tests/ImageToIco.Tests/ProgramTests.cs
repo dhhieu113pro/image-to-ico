9574b707cc04884baf4cd4e7aa66d27426b4d0f2
Hieu Dam <86464713+dhhieu113pro@users.noreply.github.com>
2026-09-05 11:02:40 +0700

    test: cover successful CLI execution

--- tests/ImageToIco.Tests/ProgramTests.cs
+++ tests/ImageToIco.Tests/ProgramTests.cs
@@ -1,3 +1,4 @@
+using ImageMagick;
 using Xunit;
 
 namespace ImageToIco.Tests;
@@ -14,5 +15,26 @@ public class ProgramTests
     public void Main_ReturnsNotImplementedExitCodeForValidArguments()
     {
         Assert.Equal(1, Program.Main(["logo.png", "app.ico"]));
+    }
+
+    [Fact]
+    public void Main_ReturnsSuccessExitCodeAndCreatesIco()
+    {
+        var temp = Path.Combine(Path.GetTempPath(), "image-to-ico-program-tests-" + Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(temp);
+        try
+        {
+            var input = Path.Combine(temp, "logo.png");
+            var output = Path.Combine(temp, "app.ico");
+            using (var image = new MagickImage(MagickColors.Blue, 64, 64))
+                image.Write(input, MagickFormat.Png);
+
+            Assert.Equal(0, Program.Main([input, output, "--sizes", "32"]));
+            Assert.True(File.Exists(output));
+        }
+        finally
+        {
+            Directory.Delete(temp, true);
+        }
     }
 }
